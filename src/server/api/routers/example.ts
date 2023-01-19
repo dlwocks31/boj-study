46e49ce21b6c6f6e33af769365162dca4f732b93
Jaechan <dlwocks31@gmail.com>
2023-01-19 12:51:13 +0900

    각 유저의 풀이 상황을 볼 수 있는 테이블 생성

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,18 +1,27 @@
+import { z } from "zod";
 import { getUserSolvedProblemIds } from "../../utils/boj";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const exampleRouter = createTRPCRouter({
-  getBoj: publicProcedure.query(async ({ ctx }) => {
-    const solvedAcClass2 = [
-      1018, 1181, 1259, 1920, 1978, 2164, 2609, 2751, 2798, 4153, 9012, 10250,
-      10814, 10816, 10828, 10845, 10866, 11050, 11650, 11866,
-    ];
+  // get list of user ids as input
+  getBoj: publicProcedure
+    .input(
+      z.object({
+        userIds: z.array(z.string()),
+      })
+    )
+    .query(async ({ input: { userIds } }) => {
+      const solvedAcClass2 = [
+        1978, 9012, 10828, 2798, 2751, 10250, 2609, 1181, 11650, 1920,
+      ];
 
-    const solved = await getUserSolvedProblemIds("goodboy302");
-    const classSolved = solved.filter((id) => solvedAcClass2.includes(id));
-    const classNotSolved = solvedAcClass2.filter(
-      (id) => !classSolved.includes(id)
-    );
-    return { classSolved, classNotSolved };
-  }),
+      const solved = await Promise.all(
+        userIds.map(async (id) => ({
+          userId: id,
+          solved: await getUserSolvedProblemIds(id),
+        }))
+      );
+
+      return { solved };
+    }),
 });
