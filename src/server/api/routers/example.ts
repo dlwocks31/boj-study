2cf86e3422cb298cd6102b9e8aa3a5d7347561cb
Jaechan <dlwocks31@gmail.com>
2023-01-20 17:28:55 +0900

    주석 삭제

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -3,7 +3,6 @@ import { getUserSolvedProblemIds } from "../../utils/boj";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const exampleRouter = createTRPCRouter({
-  // get list of user ids as input
   getBoj: publicProcedure
     .input(
       z.object({
@@ -11,10 +10,6 @@ export const exampleRouter = createTRPCRouter({
       })
     )
     .query(async ({ input: { userIds } }) => {
-      const solvedAcClass2 = [
-        1978, 9012, 10828, 2798, 2751, 10250, 2609, 1181, 11650, 1920,
-      ];
-
       const solved = await Promise.all(
         userIds.map(async (id) => ({
           userId: id,
